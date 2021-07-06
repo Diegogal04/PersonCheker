d96900dad2c0d9b37ca909d183a1a4086b9c648e
Diegogal04 <68531355+Diegogal04@users.noreply.github.com>
2021-07-06 16:53:19 -0500

    termine funcion de modalsolo falta cachar el data del usuario y ponerla en el body

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
+
+const modal = document.querySelector('.modal');
 const btnAdd = document.getElementById("add-btn");
+const btnClose = document.getElementById("close-x");
+const submitBtn = document.getElementById("submit-btn");
+
+function toglleModal() {
+    modal.classList.toggle('hidden');
+};
+
+
+btnAdd.addEventListener('click', toglleModal);
+btnClose.addEventListener('click', toglleModal);
+submitBtn.addEventListener('click', toglleModal);
+
+
+
+
+
+
+/* const btnAdd = document.getElementById("add-btn");
 const personContainer = document.getElementById("div-container");
 
 function addNew() {
@@ -7,4 +27,4 @@ function addNew() {
     personContainer.appendChild(newDiv)
 }
 
-btnAdd.addEventListener('click', addNew);
+btnAdd.addEventListener('click', addNew); */
