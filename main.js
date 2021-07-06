4abff686093bfd02fdd6248d4ca75bb281a60529
Diegogal04 <68531355+Diegogal04@users.noreply.github.com>
2021-07-06 17:07:51 -0500

    working on adding persons function

--- main.js
+++ main.js
@@ -3,15 +3,23 @@ const modal = document.querySelector('.modal');
 const btnAdd = document.getElementById("add-btn");
 const btnClose = document.getElementById("close-x");
 const submitBtn = document.getElementById("submit-btn");
+const personContainer = document.getElementById("div-container");
 
 function toglleModal() {
     modal.classList.toggle('hidden');
 };
 
+function addNewPerson() {
+    const newPerson = document.createElement('div');
+    newPerson.classList.add('div-person');
+    personContainer.appendChild(newPerson);
+    console.log('hello world')
+}
+
 
 btnAdd.addEventListener('click', toglleModal);
 btnClose.addEventListener('click', toglleModal);
-submitBtn.addEventListener('click', toglleModal);
+submitBtn.addEventListener('click', addNewPerson);
 
 
 
